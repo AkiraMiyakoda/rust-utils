9cfe3b5612e01bc7b00cb64cd70d3efe25aaffa4
Akira Miyakoda <akira.miyakoda@gmail.com>
2024-07-26 18:32:43 +0900

    Small fix in style

--- logger/src/lib.rs
+++ logger/src/lib.rs
@@ -1,4 +1,4 @@
-// Copyright © 2023 Akira Miyakoda
+// Copyright © 2024 Akira Miyakoda
 //
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
@@ -15,26 +15,33 @@ use log::Level;
 const DEAFULT_LEVEL: &str = "error";
 
 pub fn init_logger() {
-    let level: Cow<str> = std::env::var(DEFAULT_FILTER_ENV)
+    let level: Cow<_> = std::env::var(DEFAULT_FILTER_ENV)
         .map(|v| v.into())
         .unwrap_or(DEAFULT_LEVEL.into());
 
     Builder::new()
         .format(|buf, record| {
-            let (color, effect) = match record.level() {
-                Level::Error => (AnsiColor::Red, Effects::BOLD),
-                Level::Warn => (AnsiColor::Yellow, Effects::BOLD),
-                Level::Info => (AnsiColor::Green, Effects::new()),
-                Level::Debug => (AnsiColor::Blue, Effects::new()),
-                Level::Trace => (AnsiColor::White, Effects::new()),
+            let styles = {
+                let (color, effect) = match record.level() {
+                    Level::Error => (AnsiColor::Red, Effects::BOLD),
+                    Level::Warn => (AnsiColor::Yellow, Effects::BOLD),
+                    Level::Info => (AnsiColor::Green, Effects::new()),
+                    Level::Debug => (AnsiColor::Blue, Effects::new()),
+                    Level::Trace => (AnsiColor::White, Effects::new()),
+                };
+
+                (
+                    Style::new().fg_color(Some(color.into())).effects(effect),
+                    Style::new().effects(effect),
+                )
             };
-            let level_style = Style::new().fg_color(Some(color.into())).effects(effect);
-            let args_style = Style::new().effects(effect);
 
             writeln!(
                 buf,
-                "{timestamp} [{level_style}{level:<5}{level_style:#}] {args_style}{args}{args_style:#}",
-                timestamp = Local::now().format("%Y-%m-%d %H:%M:%S%.6f %Z"),
+                "{timestamp} [{st0}{level:<5}{st0:#}] {st1}{args}{st1:#}",
+                timestamp = Local::now().format("%Y-%m-%d %H:%M:%S%.3f %Z"),
+                st0 = styles.0,
+                st1 = styles.1,
                 level = record.level(),
                 args = record.args(),
             )
